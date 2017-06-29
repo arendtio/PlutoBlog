b724a22e1a70459fd060a08139252a8677bd9ace
Finn Arendt <finn-arendt-pluto@ubermail.me>
2017-06-29 11:46:28 +0200

    fix highlightjs (query 'pre code' instead of just 'pre')

--- 04_blog/js/helper.js
+++ 04_blog/js/helper.js
@@ -1,3 +1,14 @@
+function triggerEvent(name, args) {
+	if ("createEvent" in document) {
+		var evt = document.createEvent("HTMLEvents");
+		evt.initEvent(name, false, true);
+		evt.args = args
+		this.dispatchEvent(evt);
+	}
+	else
+		this.fireEvent(name, {"args": args});
+}
+
 // http://youmightnotneedjquery.com/
 function removeClass(el, className) {
 	if (el.classList)
